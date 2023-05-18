0020b278b20e028722c1a4eb37b14f81da5d92df
Vasile Pește <vasile.peste@protonmail.ch>
2023-05-18 03:02:54 +0200

    v2023.5.0

--- src/core/Mida.ts
+++ src/core/Mida.ts
@@ -37,7 +37,7 @@ class Mida {
     static readonly #installedPlugins: Map<string, MidaPlugin> = new Map();
 
     public static get version (): string {
-        return "2023.4.0";
+        return "2023.5.0";
     }
 
     public static get logger (): MidaLogger {
